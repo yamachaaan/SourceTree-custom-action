70851d9a6d09ccccb0a8ae08c664e82ed80e4bc3
yamachaaan <yamachaaan@users.noreply.github.com>
2017-05-25 13:07:27 +0900

    add some files

---
+++ mergerequest.bat
@@ -0,0 +1,10 @@
+@echo off
+set CMD=git symbolic-ref --short HEAD
+set branch=
+for /f "usebackq delims=" %%a in (`%CMD%`) do set branch=%%a
+echo %branch%
+echo %branch:~8%
+git commit --allow-empty -m"refs fixed #%branch:~8%"
+git push origin %branch%
+
+exit /b 0
